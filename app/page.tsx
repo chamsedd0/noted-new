e0ee7bf0463f0da4d8c34a04651290dc50554070
chamsedd0 <lhouijchams@gmail.com>
2024-12-03 20:10:58 +0300

    sixth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+import styled from "styled-components";
+
+const Box = styled.div`
+  
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center
+
+  a {
+    color: #383838;
+    padding: 0.5rem 1rem;
+    background-color: white;
+    border-radius: 100px;
+  }
+
+`
+
 export default function Home() {
   const { data: session } = useSession();
   console.log(session);
-  return <div></div>;
+  return <Box><Link href='/login'>Go to Login</Link></Box>;
 }
