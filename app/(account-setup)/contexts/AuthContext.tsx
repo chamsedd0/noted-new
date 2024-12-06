28a6b0029ff2c40cf9ac427684671ec9cd91708d
chamsedd0 <lhouijchams@gmail.com>
2024-12-06 10:04:06 +0300

    and another one

--- app/(account-setup)/contexts/AuthContext.tsx
+++ app/(account-setup)/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 
 // Define the AuthContextValue interface
 interface AuthContextValue {
